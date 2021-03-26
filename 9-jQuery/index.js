5aa1e8c6fd1ddf189e65d7877739cc1fca6c1d43
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-25 21:46:08 -0700

    lesson 184

--- 9-jQuery/index.js
+++ 9-jQuery/index.js
@@ -1,23 +1,31 @@
-// Without jQuery
-// for (var i = 0; i < 5; i++) {
-//     document.querySelectorAll("button")[i].addEventListener("click", function() {
-//         document.querySelector("h1").style.color = "purple";
-//     });
-// }
-
-// //With jQuery
-// $("button").click(function() {
-//     $("h1").css("color", "purple");
+// $("button").on("click", function() {
+//     $("h1").hide();
 // });
 
-// $("input").keydown(function(event) {
-//     console.log(event.key);
+// $("button").on("click", function() {
+//     $("h1").toggle();
 // });
 
-// $(document).keydown(function(event) {
-//     $("h1").text(event.key);
+// $("button").on("click", function() {
+//     $("h1").fadeOut();
 // });
 
-$("h1").on("mouseover", function() {
-    $("h1").css("color", "purple");
+// $("button").on("click", function() {
+//     $("h1").fadeToggle();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").slideUp();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").slideToggle();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").animate({opacity: 0.5});
+// });
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
 });

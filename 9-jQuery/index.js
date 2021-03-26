64fb386504d6c788066a155f2c56a2c21313ed58
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-25 21:37:58 -0700

    lesson 182

--- 9-jQuery/index.js
+++ 9-jQuery/index.js
@@ -1,1 +1,23 @@
-$("a").attr("href", "https://www.yahoo.com");
+// Without jQuery
+// for (var i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+// //With jQuery
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("input").keydown(function(event) {
+//     console.log(event.key);
+// });
+
+// $(document).keydown(function(event) {
+//     $("h1").text(event.key);
+// });
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
+});

724678d01c764d427f9360f5b42f3e927cb554c2
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-28 21:24:18 -0700

    lesson 234

--- 13-Calculator/calculator.js
+++ 13-Calculator/calculator.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello world")
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function(){
-    console.log("Server started on port 3000");
+    console.log("Server is running on port 3000.");
 });

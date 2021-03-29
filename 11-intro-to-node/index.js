8cd4457bc31f5fe7b3c8fd70811105094503bdd9
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-28 20:27:08 -0700

    lesson 224

--- 11-intro-to-node/index.js
+++ 11-intro-to-node/index.js
@@ -1,3 +1,12 @@
-const fs = require("fs");
+// const fs = requires("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// fs.copyFileSync("file1.txt", "file2.txt")
+
+var superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);

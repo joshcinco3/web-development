8cc4742b887053f98852023752eadaeeeed85bd6
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-30 21:44:34 -0700

    lesson 245-246

--- 14-Weather-Project/app.js
+++ 14-Weather-Project/app.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Oakland&appid=590e5f3d033d4135ec2f155491c8c5f6&units=imperial";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "590e5f3d033d4135ec2f155491c8c5f6";
+    const unit = "imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
     https.get(url, function(response){
-        console.log(response.statusCode);
+            response.on("data", function(data){
+                const weatherData = JSON.parse(data);
+                const temp = weatherData.main.temp;
+                const weatherDescription = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL ="http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            console.log(temp);
-            console.log(weatherDescription);
+                res.write("<p>The weather is currently " + weatherDescription + "</p>");
+                res.write("<h1>The temperature in " + query + " is " + temp + " degrees Fahrenheit.</h1>");
+                res.write("<img src=" + imageURL + "/>");
+                
+                res.send();
+            });
         });
-
-    });
-
-    res.send("Server is up and running.");
 });
 
 app.listen(3000, function() {

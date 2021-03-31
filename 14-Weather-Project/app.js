035d41d9e97c8caa5ed070d7fa287b0a4f47e1ae
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-30 21:08:36 -0700

    lesson 244

---
+++ 14-Weather-Project/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Oakland&appid=590e5f3d033d4135ec2f155491c8c5f6&units=imperial";
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            console.log(temp);
+            console.log(weatherDescription);
+        });
+
+    });
+
+    res.send("Server is up and running.");
+});
+
+app.listen(3000, function() {
+    console.log("Sever is running on port 3000.");
+});

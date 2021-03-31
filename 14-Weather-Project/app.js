adc22c03b9422d775daefe3d3687add21f79edd6
joshcinco3 <josh.cinco.3@gmail.com>
2021-03-30 22:02:04 -0700

    lesson 248 pt1

--- 14-Weather-Project/app.js
+++ 14-Weather-Project/app.js
@@ -34,5 +34,5 @@ app.post("/", function(req, res){
 });
 
 app.listen(3000, function() {
-    console.log("Sever is running on port 3000.");
+    console.log("Server is running on port 3000.");
 });

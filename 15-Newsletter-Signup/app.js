883c5e5fc87179ba28736e7d52cd0209fa6a475b
joshcinco3 <josh.cinco.3@gmail.com>
2021-04-01 22:35:53 -0700

    lesson 249

--- 15-Newsletter-Signup/app.js
+++ 15-Newsletter-Signup/app.js
@@ -12,14 +12,48 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
 
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us1.api.mailchimp.com/3.0/lists/7214bdf945" 
+
+    const options = {
+        method: "POST",
+        auth: "joshC:746bca0615b9771e9f11294099b5b0d1-us1"
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
+
+// API Key
+// 746bca0615b9771e9f11294099b5b0d1-us1
+
+// List Id
+// 7214bdf945

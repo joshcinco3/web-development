b51ec46a0b018a7a024a7144d8ec14a9568642e3
joshcinco3 <josh.cinco.3@gmail.com>
2021-04-05 21:08:24 -0700

    lessons 258

--- 15-Newsletter-Signup/app.js
+++ 15-Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const config = require(__dirname + "/config.js");
 
 const app= express();
 
@@ -60,6 +61,6 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(process.env.PORT, function() {
+app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });

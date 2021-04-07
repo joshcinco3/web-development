e706c0449d652d8755543c78738ed2de456dbb27
joshcinco3 <josh.cinco.3@gmail.com>
2021-04-06 22:44:25 -0700

    271

--- 18-todolist-v1/app.js
+++ 18-todolist-v1/app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -19,16 +20,34 @@ app.get("/", function(req, res){
     };
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {day: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
 
-    items.push(item);
-
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req,res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
